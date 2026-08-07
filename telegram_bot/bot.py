dc4ec64152c4733bcd38943e77b0d486869647dc
butka753 <d1366466@gmail.com>
2026-08-07 20:48:04 +0700

    Add files via upload

---
+++ telegram_bot/bot.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, types
+import asyncio
+import os
+
+TOKEN = os.getenv("8875824495:AAGcaII9dC54zhTvfgbxCsgj7JIizZpThjc")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(f"Ты сказал: {message.text}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
